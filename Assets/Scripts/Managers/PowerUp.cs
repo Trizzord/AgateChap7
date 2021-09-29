3e36c706540bfe59de3bfa8e50ef5df15054f79c
Trizzord <trizzordfullfree@gmail.com>
2021-09-29 19:41:54 +0700

    Tugas Agate Chapter 7

---
+++ Assets/Scripts/Managers/PowerUp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PowerUpType
+{
+    healthUp,
+    speedUp
+}
+
+public class PowerUp : MonoBehaviour
+{
+    public PowerUpType powerType;
+    public float amount;
+    public float disappearTime;
+
+    private void Start()
+    {
+        Destroy(this.gameObject, disappearTime);
+    }
+}
